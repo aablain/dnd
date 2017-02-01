816564f4ffbfdd47bf51daabe23371610da1b4a4
Austin Blain <austin.a.blain@gmail.com>
2017-01-31 21:02:22 -0800

    remove api key and add some styling

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyDQvBEhzPS1l6Zt7aIBXFGMeeHYgJrScLs",
-   authDomain: "dndroster.firebaseapp.com",
-   databaseURL: "https://dndroster.firebaseio.com",
-   storageBucket: "dndroster.appspot.com",
-   messagingSenderId: "482732073184"
-}
